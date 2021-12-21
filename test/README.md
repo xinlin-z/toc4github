a2897cf6120032123bf4848a912baf9f2cad9265
xinlin <xinlin.zhang@hotmail.com>
2021-12-21 16:45:06 +0800

    add a placeholder for automatic replace, wrap all the stuff up for the first version!

---
+++ test/README.md
@@ -0,0 +1,46 @@
+# Test
+## header
+### head 3
+#### head 4
+##### head 5
+###### head 6
+####### not a head 7
+
+# _abc
+# a & b & c
+# a       c
+# a ( c )
+# a(c)
+# a.,.,!@#$%^&*().,.7
+
+# *italic 12345*
+# **bold 12345**
+# _italic2 12345_
+# __bold2 12345__
+# ~cross out~
+# ~~cross out 2~~
+# **~bold cross out~**
+# __~~blod cross out 2~~__
+
+Now we set placeholder below, but you can not see it!
+
+* [Test](#Test)
+    * [header](#header)
+        * [head 3](#head-3)
+            * [head 4](#head-4)
+                * [head 5](#head-5)
+                    * [head 6](#head-6)
+* [_abc](#_abc)
+* [a & b & c](#a--b--c)
+* [a       c](#a-------c)
+* [a ( c )](#a--c-)
+* [a(c)](#ac)
+* [a.,.,!@#$%^&*().,.7](#a7)
+* [*italic 12345*](#italic-12345)
+* [**bold 12345**](#bold-12345)
+* [_italic2 12345_](#italic2-12345)
+* [__bold2 12345__](#bold2-12345)
+* [~cross out~](#cross-out)
+* [~~cross out 2~~](#cross-out-2)
+* [**~bold cross out~**](#bold-cross-out)
+* [__~~blod cross out 2~~__](#blod-cross-out-2)
--- tocy.py
+++ tocy.py
@@ -6,8 +6,13 @@ with open(sys.argv[1]) as f:
     lines = f.readlines()
 
 
+toc = ''
+pos = -1
 skip = 0
-for line in lines:
+for i,line in enumerate(lines):
+    # skip empty line
+    if line.rstrip('\n').strip() == '':
+        continue
     # skip code block started with 4 space
     if line.startswith(' '*4):
         continue
@@ -15,6 +20,10 @@ for line in lines:
     if line.startswith('```'):
         skip = abs(skip-1)
     if skip: continue
+    # try to find placeholder and skip
+    if line.strip().lower() == '[tocy]':
+        pos = i
+        continue
     # search and print out #+ title
     if strs:=re.match(r'\s*(#+)(.*)',line):
         if (htl:=len((ht:=strs.groups()[0]))) > 6:  # max head level is 6
@@ -25,6 +34,15 @@ for line in lines:
             rest = rest[:a.start()] + a.groups()[1] + rest[a.end():]
         rest = re.sub(r'\s', '-', rest)  # space --> -
         rest = re.sub(r'[^-\w]', '', rest)  # squeeze other chars
-        print(''.join((' '*(htl-1)*4, '* ',
-                       '[',strs.groups()[1].strip(),'](#', rest,')')))
+        toc += ''.join((' '*(htl-1)*4, '* ',
+                       '[',strs.groups()[1].strip(),'](#', rest,')')) + '\n'
+
+
+if pos != -1:
+    # There is a \n originally, the last \n in toc is used here.
+    lines[pos] = toc
+    with open(sys.argv[1], 'w') as f:
+        f.write(''.join(lines))
+else:
+    print(toc.rstrip('\n'))
 
